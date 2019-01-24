a7857f7e6da2e2f059259ffa0feff2106413ced8
Aleksandra <a.tsyrlina@gmail.com>
2019-01-24 19:31:18 +0100

    pas fini

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,36 @@
 
+require 'pry'
+
+
 class Game
-  #TO DO : la classe a plusieurs attr_accessor: le current_player (égal à un objet Player), le status (en cours, nul ou un objet Player s'il gagne), le Board et un array contenant les 2 joueurs.
+    attr_accessor :current_player, :status, :Board, :players
+  #TO DO : la classe a plusieurs : le  (égal à un objet Player), le  (en cours, nul ou un objet Player s'il gagne), le Board et un array contenant les 2 joueurs.
 
   def initialize
+      @players_name = welcome
+      
+      @caseboard_hash = create_caseboard
+      
+      @status = "on-going"
+      
+      @current_player = @player_1
+      
     #TO DO : créé 2 joueurs, créé un board, met le status à "on going", défini un current_player
   end
 
+
   def turn
+      puts @caseboard_hash
+      @choix = choice.downcase
+      if @caseboard_hash["#{@choix}"] != " "
+          puts "essaie une case vide."
+      else @caseboard_hash("#{@choix}") = @player_value
+      end
+      
+      puts "Voici le tableau:"
+      puts @caseboard_hash
+
+      end
     #TO DO : méthode faisant appelle aux méthodes des autres classes (notamment à l'instance de Board). Elle affiche le plateau, demande au joueur ce qu'il joue, vérifie si un joueur a gagné, passe au joueur suivant si la partie n'est pas finie.
   end
 
@@ -18,4 +42,33 @@ class Game
     # TO DO : permet l'affichage de fin de partie quand un vainqueur est détecté ou si il y a match nul
   end    
 
+
+
+
+
+def welcome
+    puts "bonjour! bienvenue sur le super jeu de morpion, le jeu le plus strategique du monde! quel est le nom du joueur 1?"
+    puts "ton nom >"
+    player_1_name = gets.chomp
+    puts "super #{player_1_name}! maintenant donnes le nom du joueur 2!"
+    puts "ton nom >"
+    player_2_name = gets.chomp
+    puts "c'est parti mon kiki! #{player_1_name}, tu va jouer avec les X et #{player_2_name} tu va jouer avec les O"
+    return @players = [player_1_name, player_2_name]
 end
+
+def choice
+    puts "vas-y, fais ton choix"
+    puts "case >"
+    return @choix = gets.chomp.downcase
+    
+end
+
+def create_caseboard
+    return @caseboard_hash = {"a1" =>" ", "b1" => " ", "c1" => " ", "a2" =>" ", "b2" => " ", "c2" => " ", "a3" =>" ", "b3" => " ", "c3" => " "}
+end
+
+
+new_game = Game.new
+@status = "on-going"
+new_game.turn
--- lib/show.rb
+++ lib/show.rb
@@ -1,8 +1,24 @@
-
+require './board'
+require './game'
+require 'pry'
 class Show
 
-  def show_board(board)
+  def show_board
+      puts "le board"
+      #puts "    A    B    C"
+      #puts "  ----------------"
+      #puts "1 |#{caseboard_hash(a1)}|#{b1}|#{c1}|"
+      #puts "  ----------------"
+      #puts "2 |#{a2}|#{b2}|#{c2}|"
+      #puts "  ----------------"
+      #puts "3 |#{a3}|#{b3}|#{c3}|"
+      #puts "  ----------------"
     #TO DO : affiche sur le terminal l'objet de classe Board en entrée. S'active avec un Show.new.show_board(instance_de_Board)
   end
 
+  
+  
 end
+
+
+
